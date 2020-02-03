b186e92d5908064abdf8e2324fa00fbcdc507ef5
souvikroy20 <roysouvik304@gmail.com>
2020-02-03 17:55:35 +0530

    Verify Modusle Dashboard Changes

--- src/app/verify/verify.module.ts
+++ src/app/verify/verify.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VerifyRoutingModule } from './verify-routing.module';
 import { VerifyComponent } from './verify.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 
 @NgModule({
-  declarations: [VerifyComponent],
+  declarations: [VerifyComponent, DashboardComponent],
   imports: [
     CommonModule,
     VerifyRoutingModule
