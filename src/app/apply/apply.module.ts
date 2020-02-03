6dd429b7124677ffe158030e0dd3b298fa6df15a
Narayan SIngh Solanki <32158848+hritiksingh580@users.noreply.github.com>
2020-02-03 09:51:59 +0530

    Dashboard for Apply Module (#3)
    * Dashboard for Apply Module
    
    * Modified Dashboard for Apply Module

--- src/app/apply/apply.module.ts
+++ src/app/apply/apply.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApplyRoutingModule } from './apply-routing.module';
 import { ApplyComponent } from './apply.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 
 @NgModule({
-  declarations: [ApplyComponent],
+  declarations: [ApplyComponent, DashboardComponent],
   imports: [
     CommonModule,
     ApplyRoutingModule
